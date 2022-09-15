daaf4469e4e4a706f9ea1861d3886f90cbb7f72f
Misha Brenzovich <mishabren89@gmail.com>
2022-09-15 18:02:36 +0300

    operatoruTaCiklu

--- project.js
+++ project.js
@@ -1,19 +1,81 @@
-const numberOfFilms = +prompt('Кіко кіна ти вже відів', '')
+'use strict'
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-}
+// const numberOfFilms = +prompt('Кіко кіна ти вже відів', '')
 
-const a = prompt('Один з останніх фільмів', ''),
-  b = +prompt('Оцініть фільм', ''),
-  c = prompt('Один з останніх фільмів', ''),
-  d = +prompt('Оцініть фільм', '')
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// }
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+// const a = prompt('Один з останніх фільмів', ''),
+//   b = +prompt('Оцініть фільм', ''),
+//   c = prompt('Один з останніх фільмів', ''),
+//   d = +prompt('Оцініть фільм', '')
 
-console.log(personalMovieDB)
+// personalMovieDB.movies[a] = b
+// personalMovieDB.movies[c] = d
+
+// console.log(personalMovieDB)
+
+// або || = true
+// і && = folse
+
+// console.log(NaN || 2 || undefined)
+
+// console.log(NaN && 2 && undefined)
+
+// console.log(1 || 2 || 3)
+
+// console.log(!1 || (2 && !3))
+
+// console.log(25 || (null && !3))
+
+// console.log(NaN || (null && !3) || undefined || 5)
+
+// console.log(NaN || (null && !3 && undefined) || 5)
+
+// console.log((5 === 5 && 3 > 1) || 5)
+
+// Цикли
+
+// let num = 50
+
+// while (num < 55) {
+//   console.log(num)
+
+//   num++
+// }
+
+// let num = 50
+
+// do {
+//   console.log(num)
+//   num++
+// } while (num < 55)
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i)
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num)
+//     num++
+//   }
+
+// for (let i = 1; i < 8; i++) {
+//   if (i === 6) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     // break
+//     continue
+//   }
+//   console.log(i)
+// }

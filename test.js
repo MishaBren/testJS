051f65f6813ef1bdece77527131b3f638c472511
Misha Brenzovich <mishabren89@gmail.com>
2022-09-15 13:48:48 +0300

    practika1

--- test.js
+++ test.js
@@ -1,41 +1,41 @@
-const userName = 'Misha'
-console.log(userName)
+// const userName = 'Misha'
+// console.log(userName)
 
-let year = 2022
+// let year = 2022
 
-let userAge = year - 1989
+// let userAge = year - 1989
 
-let nameAge = userName + ' ' + userAge
-console.log(nameAge)
+// let nameAge = userName + ' ' + userAge
+// console.log(nameAge)
 
-const storeName = 'Rozetka'
-const storeDescription = {
-  budget: 10000,
-  employees: ['Misha', 'Vitja', 'Ostap'],
-}
-storeDescription.products = {
-  phone: 5000,
-  notebook: 20000,
-}
-storeDescription.open = true
-console.log(storeDescription)
+// const storeName = 'Rozetka'
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ['Misha', 'Vitja', 'Ostap'],
+// }
+// storeDescription.products = {
+//   phone: 5000,
+//   notebook: 20000,
+// }
+// storeDescription.open = true
+// console.log(storeDescription)
 
-alert('Hello User')
+// alert('Hello User')
 
-const result = confirm('Are you here?')
-console.log(result)
+// const result = confirm('Are you here?')
+// console.log(result)
 
-const answer = prompt('Вам є 18?', '33')
-console.log(answer)
+// const answer = prompt('Вам є 18?', '33')
+// console.log(answer)
 
-const answers = []
-answers[0] = prompt('Ваше імя?', '')
-answers[1] = prompt('Ваше прізвище', '')
-answers[2] = prompt('Скільки вам років', '')
-document.write(answers)
-;('use sctrict')
-const category = 'toys'
-console.log(`https://someurl.com/${catgory}/5`)
+// const answers = []
+// answers[0] = prompt('Ваше імя?', '')
+// answers[1] = prompt('Ваше прізвище', '')
+// answers[2] = prompt('Скільки вам років', '')
+// document.write(answers)
+// ;('use sctrict')
+// const category = 'toys'
+// console.log(`https://someurl.com/${catgory}/5`)
 
-const user = 'ivan'
-alert(`Привет, ${user}`)
+// const user = 'ivan'
+// alert(`Привет, ${user}`)

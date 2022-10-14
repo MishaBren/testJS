5bc965603caf03bafa2058ae6b9c851913785c18
Mikhailo <mishabren89@gmail.com>
2022-10-14 13:40:46 +0300

    14.10

--- test.js
+++ test.js
@@ -1,41 +1,85 @@
-const userName = 'Misha'
-console.log(userName)
+// const userName = 'Misha'
+// console.log(userName)
+// // -----------------------
 
-let year = 2022
 
-let userAge = year - 1989
+// let year = 2022
 
-let nameAge = userName + ' ' + userAge
-console.log(nameAge)
+// let userAge = year - 1989
 
-const storeName = 'Rozetka'
-const storeDescription = {
-  budget: 10000,
-  employees: ['Misha', 'Vitja', 'Ostap'],
-}
-storeDescription.products = {
-  phone: 5000,
-  notebook: 20000,
-}
-storeDescription.open = true
-console.log(storeDescription)
+// let nameAge = userName + ' ' + userAge
+// console.log(nameAge)
+// // -----------------------
 
-alert('Hello User')
 
-const result = confirm('Are you here?')
-console.log(result)
+// const storeName = 'Rozetka'
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ['Misha', 'Vitja', 'Ostap'],
+// }
+// storeDescription.products = {
+//   phone: 5000,
+//   notebook: 20000,
+// }
+// storeDescription.open = true
+// console.log(storeDescription)
+// // -------------------------
 
-const answer = prompt('Вам є 18?', '33')
-console.log(answer)
 
-const answers = []
-answers[0] = prompt('Ваше імя?', '')
-answers[1] = prompt('Ваше прізвище', '')
-answers[2] = prompt('Скільки вам років', '')
-document.write(answers)
-;('use sctrict')
-const category = 'toys'
-console.log(`https://someurl.com/${catgory}/5`)
+// alert('Hello User')
 
-const user = 'ivan'
-alert(`Привет, ${user}`)
+// const result = confirm('Are you here?')
+// console.log(result)
+
+// const answer = prompt('Вам є 18?', '33')
+// console.log(answer)
+
+// const answers = []
+// answers[0] = prompt('Ваше імя?', '')
+// answers[1] = prompt('Ваше прізвище', '')
+// answers[2] = prompt('Скільки вам років', '')
+// document.write(answers)
+// ;('use sctrict')
+// const category = 'toys'
+// console.log(`https://someurl.com/${catgory}/5`)
+
+// const user = 'ivan'
+// alert(`Привет, ${user}`)
+
+// let result = "";
+// const length = 7
+
+//  for (let i=1; i < length; i++) {
+//   for (let j=0; j<i; j++) {
+//     result += "*"
+//   }
+//   result += '\n'
+//  }
+// console.log(result)
+// // ---------------------
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`firstLevel: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`secondLevel: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//         console.log(`tirdLevel: ${k}`);
+//         }
+//     }
+// }
+// // --------------------
+
+// for (let i = 5; i < 11; i++) {
+//         console.log(i);
+//     }
+// --------------------------
+
+// for (let i = 20; i > 9; i--) {
+//     console.log(i);
+// }
+// ---------------------------------
+
+for (let i = 2; i = 10; i++) {
+        console.log(i);
+    }

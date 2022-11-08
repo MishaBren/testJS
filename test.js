3a16210048c24bf1cc40dc3b1af8985eae539954
Misha Brenzovich <mishabren89@gmail.com>
2022-11-08 18:55:58 +0200

    pzdc

--- test.js
+++ test.js
@@ -1,60 +1,179 @@
-// const userName = 'Misha'
-// console.log(userName)
+// Логічні Оперетори
 
-// let year = 2022
+// const hamburger = 5
+// const fries = 5
 
-// let userAge = year - 1989
-
-// let nameAge = userName + ' ' + userAge
-// console.log(nameAge)
-
-// const storeName = 'Rozetka'
-// const storeDescription = {
-//   budget: 10000,
-//   employees: ['Misha', 'Vitja', 'Ostap'],
+// if (hamburger && fries) {
+//   console.log('Я ситий')
 // }
-// storeDescription.products = {
-//   phone: 5000,
-//   notebook: 20000,
+
+// ----------------
+
+// const hamburger = 3
+// const fries = 1
+// const kola = 1
+// if (hamburger === 3 && kola === 1 && fries) {
+//   console.log('Всі ситі')
+// } else {
+//   console.log('Mи йдемо геть')
 // }
-// storeDescription.open = true
-// console.log(storeDescription)
 
-// alert('Hello User')
+// ---------------------------
 
-// const result = confirm('Are you here?')
+// const hamburger = 1
+// const fries = 0
+// const kola = 0
+// if (hamburger && (kola || fries)) {
+//   console.log('Всі ситі')
+// } else {
+//   console.log('Mи йдемо геть')
+// }
+
+// --------Цикли--------------
+
+// let num = 5
+// while (num < 11) {
+//   console.log(num)
+//   num++
+// }
+
+// ----------------------
+
+// let num = 20
+// do {
+//   console.log(num)
+//   num--
+// } while (num > 9)
+
+// ----------------------------
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i)
+// }
+
+// ------------------
+
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+// ----------------------------------
+
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     continue
+//   }
+//   console.log(i)
+// }
+
+// ------Цикли в Циклах-------------
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i)
+//   for (let j = 3; j < 5; j++) {
+//     console.log(j)
+//   }
+// }
+
+//------------------------------------
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+
+// let result = ''
+// const lenght = 7
+
+// for (let i = 1; i < lenght; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += '*'
+//   }
+//   result += '\n'
+// }
+
 // console.log(result)
 
-// const answer = prompt('Вам є 18?', '33')
-// console.log(answer)
+// ------------------------------
 
-// const answers = []
-// answers[0] = prompt('Ваше імя?', '')
-// answers[1] = prompt('Ваше прізвище', '')
-// answers[2] = prompt('Скільки вам років', '')
-// document.write(answers)
-// ;('use sctrict')
-// const category = 'toys'
-// console.log(`https://someurl.com/${catgory}/5`)
-
-// const user = 'ivan'
-// alert(`Привет, ${user}`)
-
-// {
-//     "workbench.colorTheme": "Default Dark+",
-//     "liveServer.settings.donotShowInfoMsg": true,
-//     "git.confirmSync": false,
-//     "files.autoSave": "onFocusChange",
-//     "[html]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
-//     "editor.formatOnSave": true,
-//     "[javascript]": {
-//         "editor.defaultFormatter": "esbenp.prettier-vscode"
-//     },
-//     "explorer.confirmDelete": false,
-//     "window.zoomLevel": 1,
-//     "editor.codeActionsOnSave": {
-
+// first: for (let i = 1; i < 3; i++) {
+//   console.log(`First Level: ${i}`)
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`Second Level: ${j}`)
+//     for (let k = 0; k < 3; k++) {
+//       if (k === 2) continue first
+//       console.log(`Third Level: ${k}`)
 //     }
+//   }
 // }
+
+// ------------------------------------------
+
+// first: for (let i = 1; i < 3; i++) {
+//   console.log(`First Level: ${i}`)
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`Second Level: ${j}`)
+//     for (let k = 0; k < 3; k++) {
+//       if (k === 2) break first
+//       console.log(`Third Level: ${k}`)
+//     }
+//   }
+// }
+
+// ------------------------------------
+
+// for (let i = 20; i > 10; i--) {
+//   if (i === 13) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+// ----------------------------------
+
+// for (let i = 2; i < 11; i += 2) {
+//   console.log(i)
+// }
+
+// --------------------------
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue
+//   } else {
+//     console.log(i)
+//   }
+// }
+
+// let mass = []
+// let num = 3
+// while (num < 16) {
+//   mass = num
+//   console.log(mass)
+//   num += 2
+// }
+
+// ------------------
+
+// const arrayOfNumbers = []
+
+// for (let i = 5; i < 11; i++) {
+//   arrayOfNumbers[i - 5] = i
+// }
+// console.log(arrayOfNumbers)
+
+// return arrayOfNumbers
+
+// ----------------
+const arr = [3, 5, 8, 16, 20, 23, 50]
+const result = []
+for (let i = 0; i < arr.length; i++) {
+  result[i] = arr[i]
+}
+console.log(result)
